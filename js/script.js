5baecb3dc6f7b28f65426b863db3f02bc9a51733
disjfa <disjfa@disjfa.nl>
2017-08-08 22:25:11 +0000

    Update 2017-08-08T22:25:09.466Z

--- js/script.js
+++ js/script.js
@@ -12,12 +12,12 @@ Vue.component('tour-component', {
     }
   },
   methods: {
-    getDate (date) {
+    getDate: function (date) {
       var date = new Date(date);
       return date.toLocaleDateString();
     },
   },
-  mounted () {
+  mounted: function () {
     var self = this
     $.ajax('https://rest.bandsintown.com/artists/HappyThoughts/events?app_id=eyJUb2tlblR5cGUiOiJBUEkiLCJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJmMmJlM2FiYS01ODAzLTQyNGMtOWMxNi0xOTlhMDIyNzAwOGMiLCJpYXQiOjE1MDIyMjg4MzB9.wj6rD0wS0ct-aEm2Em0BqhztwXEFvEWp76X-cNyXAG76nEVYcmz4Hmd6VxLr_oMNV9HGctRxQeqhlRxSs0A1fg')
       .then(function (response) {
